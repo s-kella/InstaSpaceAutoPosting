90038ddd27d10cc7a7db50c5b4b5cb906a8590b5
s-kella <52741545+s-kella@users.noreply.github.com>
2020-09-09 16:07:01 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -1,42 +1,35 @@
 import requests
 import os
 import argparse
-
-
-def download(url, filename):
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-
-
-def find_extension(url):
-    url = url.split('.')
-    return url[-1]
+import pathlib
+import utils
 
 
 def fetch_hubble_picture(collection_name):
-    url = 'http://hubblesite.org/api/v3/images'
+    url = 'http://hubblesite.org/api/v3/'
     payload = {'collection_name': collection_name}
-    response = requests.get(url, params=payload)
+    response = requests.get(f'{url}images', params=payload)
     response.raise_for_status()
     for image_info in response.json():
-        image_info_url = url.strip('s') + '/' + str(image_info['id'])  # ссылка на инфо и ссылки со всеми размерами
+        image_info_url = f'{url}image/{str(image_info["id"])}'
         response = requests.get(image_info_url)
         response.raise_for_status()
-        image_url = 'http:' + response.json()["image_files"][1]['file_url']  # ссылка на самую большую картинку
-        filename = str(image_info['id']) + '.' + find_extension(image_url)
-        download(image_url, filename)
+        image_url = f"http:{response.json()['image_files'][1]['file_url']}"
+        filename = str(image_info['id']) + os.path.splitext(image_url)[1]
+        utils.download_photo(image_url, filename)
 
 
-path = os.getcwd()  # получить текующую папку
-files = os.listdir(path)  # список всех файлов
-dir_name = 'images'
-if dir_name not in files:
-    os.mkdir(dir_name)  # создать папку
-path += '\\' + dir_name
-os.chdir(path)  # сменить папку
-parser=argparse.ArgumentParser()
-parser.add_argument('collection',help='For example: "holiday_cards", "wallpaper", "spacecraft", "news", "printshop", "stsci_gallery"')
-args = parser.parse_args()
-fetch_hubble_picture(args.collection)
+def main():
+    dir_name = 'images'
+    os.makedirs(dir_name, exist_ok=True)
+    path = pathlib.Path.cwd() / dir_name
+    os.chdir(path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('collection',
+                        help='For example: "holiday_cards", "wallpaper", "spacecraft", "news", "printshop", "stsci_gallery"')
+    args = parser.parse_args()
+    fetch_hubble_picture(args.collection)
+
+
+if __name__ == '__main__':
+    main()
