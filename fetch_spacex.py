eac692832504aca8c0e10682d816ac6b6ba3d978
s-kella <52741545+s-kella@users.noreply.github.com>
2020-09-09 16:07:53 +0300

    Update fetch_spacex.py

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,33 +1,25 @@
 import requests
 import os
-
-
-def download(url, filename):
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-
-
-def find_extension(url):
-    url = url.split('.')
-    return url[-1]
+import pathlib
+import utils
 
 
 def fetch_spacex_last_launch():
-    url = 'http://api.spacexdata.com/v3/launches/latest'  # откуда беру ссылки на фото
+    url = 'http://api.spacexdata.com/v3/launches/latest'
     response = requests.get(url)
     response.raise_for_status()
     for image_number, image_url in enumerate(response.json()['links']['flickr_images']):
-        filename = 'spacex' + str(image_number + 1) + '.jpg'
-        download(image_url, filename)
+        filename = f'spacex{str(image_number + 1)}.jpg'
+        utils.download_photo(image_url, filename)
 
 
-path = os.getcwd()
-files = os.listdir(path)
-dir_name = 'images'
-if dir_name not in files:
-    os.mkdir(path)
-path += '\\' + dir_name
-os.chdir(path)
-fetch_spacex_last_launch()
+def main():
+    dir_name = 'images'
+    os.makedirs(dir_name, exist_ok=True)
+    path = pathlib.Path.cwd() / dir_name
+    os.chdir(path)
+    fetch_spacex_last_launch()
+
+
+if __name__ == '__main__':
+    main()
