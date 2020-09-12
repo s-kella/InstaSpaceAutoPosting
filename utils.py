8706b3cc80c2c76f49832140ca70c25c0d8ed344
s-kella <52741545+s-kella@users.noreply.github.com>
2020-09-12 09:29:33 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def download_photo(url, filename):
-    response = requests.get(url, verify=False)  # получить ответ сайта в переменную response
+    response = requests.get(url, verify=False)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
